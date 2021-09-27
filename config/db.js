6e9e8f0a3039cfda63ef930c30d3572318b0b848
diganta15 <ddas461598@gmail.com>
2021-09-27 16:02:21 +0530

    Connected to MongoDb Atlas and added morgan for logging

--- config/db.js
+++ config/db.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async()=>{
+const connectDB = async ()=>{
     try{
-
+     
+        const conn = await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+           
+        });
+        // console.log('connected');
+        console.log(`MongoDB connected: ${conn.connection.host}`)
     }catch(err){
-        
+        console.log(err);
+        process.exit(1);
     }
 }
+
+module.exports = connectDB;
